40802a687570f89074cf67bd8e965a0b6d41085c
lee-ahreum-f <leeahreum@limefriends.com>
2026-08-11 15:03:31 +0900

    chore: 메인 업데이트

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-import './App.css'
+import { Button } from '@shared/src'
 
 function App() {
-  return <div>서브모듈을 연결한 메인 프로젝트</div>
+  return (
+    <div>
+      서브모듈을 연결한 메인 프로젝트
+      <Button>버튼</Button>
+    </div>
+  )
 }
 
 export default App
