2d2f1fc52ad85664650907620d0e0b655e8efac2
lee-ahreum-f <leeahreum@limefriends.com>
2026-08-11 15:14:16 +0900

    feat: 테스트 유틸 연결

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { Button } from '@shared/src'
+import { test } from '@shared/src'
 
 function App() {
   return (
     <div>
       서브모듈을 연결한 메인 프로젝트
-      <Button>버튼</Button>
+      <Button onClick={test}>버튼</Button>
     </div>
   )
 }
